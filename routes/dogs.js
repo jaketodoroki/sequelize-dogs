9fde756d7be123a9065bc85231a23443b0e16754
JakeTodoroki <taco7897@gmail.com>
2023-02-14 16:23:32 -0700

    create and index routes and controllers

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
 const dogsCtrl = require('../controllers/dogs.js')
 
+router.get('/', dogsCtrl.index)
+router.post('/', dogsCtrl.create)
+
 module.exports = router
