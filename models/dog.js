d0791197ff0f7c1101c9ece7f4886e83ed53da1f
JakeTodoroki <taco7897@gmail.com>
2023-02-16 17:13:45 -0700

    define cleaning and dog association

--- models/dog.js
+++ models/dog.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Dog.hasMany(models.Cleaning, {
+        foreignKey: 'dogId',
+        as: 'cleanings'
+      })
     }
   }
   Dog.init({
